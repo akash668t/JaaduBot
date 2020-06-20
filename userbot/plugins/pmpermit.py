8290c94836b6dff3c60c3c074b53d2451d30a01c
Amberyt <59554677+Amberyt@users.noreply.github.com>
2020-06-20 23:54:27 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -11,11 +11,11 @@ PREV_REPLY_MESSAGE = {}
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
+USER_BOT_WARN_ZERO = "`Spam Karega Dalle Ja Gaand Mara Ab.` "
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=948408212)\n\n"
-                    "`Hello, this is Protected Security Service.You have found your way here to my master,`"
+                    "`Yo Hello Sir Pls Dont Spam,`"
                     f"{DEFAULTUSER}`'s inbox.\n\n"
-                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
+                    "Apna Kaam Aur Naam Dono Likh Aur Nikal Smja.`\n\n"
                     "** Send** `/start` ** so that we can decide why you're here.**")
 
 
