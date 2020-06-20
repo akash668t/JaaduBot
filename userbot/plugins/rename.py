edc6ddc36f6c4934ebbbfe4003f275a175f91073
Amberyt <59554677+Amberyt@users.noreply.github.com>
2020-06-20 16:52:22 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -167,7 +167,7 @@ async def _(event):
                     event.chat_id,
                     downloaded_file_name,
                     thumb=thumb,
-                    caption="reuploaded by [GujjuBot](https://www.github.com/japarmar/GujjuBot",
+                    caption="reuploaded by [GujjuBot](https://www.github.com/Amberyt/JaaduBot",
                     force_document=False,
                     allow_cache=False,
                     reply_to=event.message.id,
